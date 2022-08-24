1a50b059b557b67cec72f42a51ea883aee11f2ac
KLAU-S <enayatkareem99@gmail.com>
2022-08-25 00:07:19 +0530

    modified Procfile

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'todo',
 ]
 
 MIDDLEWARE = [
